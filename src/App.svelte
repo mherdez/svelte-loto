71f478501a15583c7896e1668212643b91f6a002
Herdez <mherdez@users.noreply.github.com>
2023-04-18 10:00:15 -0600

    loteria campechana

--- src/App.svelte
+++ src/App.svelte
@@ -15,18 +15,18 @@
 
   const iniBingo = () => {
     const ciclo = setInterval(() => {
-    const ball = +bingo.splice(0, 1);
-    bolas = [ball, ...bolas];
-    if (cartilla.includes(ball)) bolasSacadas = [...bolasSacadas, ball];
-    if (bolasSacadas.length >= 9) {
-      confetti();
-      winner = true;
-      clearInterval(ciclo);
-    }
-  }, 1000);
-  }
+      const ball = +bingo.splice(0, 1);
+      bolas = [ball, ...bolas];
+      if (cartilla.includes(ball)) bolasSacadas = [...bolasSacadas, ball];
+      if (bolasSacadas.length >= 9) {
+        confetti();
+        winner = true;
+        clearInterval(ciclo);
+      }
+    }, 1000);
+  };
 
-  iniBingo()
+  iniBingo();
 
   const newGame = () => {
     cartilla = createNumbers().slice(0, 9);
@@ -38,11 +38,14 @@
   };
 </script>
 
-<h1>Bingo</h1>
+<h1>Lotería Campechana</h1>
 <main>
   <section>
     {#each cartilla as item}
-      <div class="celda"><span class={bolas.includes(item) ? 'ok' : ''}>{item}</span></div>
+      <div class="celda">
+        <div class={bolas.includes(item) ? 'ok' : ''}></div>
+        <img src="./assets/{item}.png" alt="img{item}" />
+      </div>
     {/each}
   </section>
   <div class="bingo">
@@ -53,7 +56,7 @@
 
   {#if winner}
     <div class="winner">
-      <p>BINGO!!!</p>
+      <p>LOTERÍA!!!</p>
       <div class="newGame" on:click={newGame}>New game</div>
     </div>
   {/if}
@@ -61,14 +64,15 @@
 
 <style>
   h1 {
-    text-align: center;;
+    text-align: center;
+    letter-spacing: .1rem;
   }
   main {
     display: flex;
-    flex-direction: column;;
+    flex-direction: column;
     justify-content: center;
     align-items: start;
-    padding: .5rem 0;
+    padding: 0.5rem 0;
   }
   .bingo {
     /* order: 2; */
@@ -78,7 +82,7 @@
   }
   .bingo .first {
     grid-column: 1 / -1;
-    border: 10px solid yellow;
+    border: 10px solid rgb(255, 200, 0);
     font-size: 3rem;
     inline-size: 3rem;
     height: 3rem;
@@ -87,7 +91,7 @@
     justify-self: center;
     align-self: center;
     margin-bottom: 1rem;
-    background-color: rgb(102, 0, 203);
+    background-color: rgb(157, 90, 1);
     font-weight: bold;
     text-shadow: 5px 5px 10px black;
   }
@@ -107,32 +111,44 @@
   }
   section {
     display: grid;
-    grid-template-columns: repeat(3, 130px);
+    grid-template-columns: repeat(3, 17 0px);
     margin: 0 auto;
   }
   .celda {
     height: 130px;
     /* order: 1; */
-    border: 3px solid rgb(255, 255, 0);
+    /* border: 3px solid rgb(255, 255, 0); */
+    border: 5px solid rgba(0, 0, 0, 0.736);
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: 3rem;
     text-shadow: 3px 3px 10px rgba(0, 0, 0, 0.736);
+    position: relative;
+    border-radius: 20px;
+
+  }
+  .celda img {
+    height: 100%;
+    width: 100%;
   }
   .ok {
-    background-color: rgb(73, 16, 131);
-    border: 5px solid rgb(255, 255, 0);
-    border-radius: 50%;
-    width: 60%;
+    position: absolute;
+    /* background-color: rgb(73, 16, 131);
+    border: 5px solid rgb(255, 255, 0); */
+    /* border-radius: 50%; */
+    width: 70%;
     height: auto;
     aspect-ratio: 1/1;
     display: flex;
     justify-content: center;
     align-items: center;
     font-weight: bold;
-    box-shadow: 5px 5px 10px 2px black;
-    font-size: 2.5rem;
+    /* box-shadow: 5px 5px 10px 2px black; */
+    /* font-size: 2.5rem; */
+    background-image: url('./assets/frijol.png');
+    background-position: center;
+    background-size: cover;
   }
   .winner {
     width: 100%;
@@ -150,128 +166,129 @@
   }
   .winner p {
     font-size: 3.8rem;
-    letter-spacing: .5rem;
+    letter-spacing: 0.5rem;
     font-weight: bolder;
     font-family: Arial, Helvetica, sans-serif;
-    -webkit-text-stroke: 0.1rem white;
+    -webkit-text-stroke: 0.1rem black;
     text-shadow: 5px 5px 15px black;
-    color: red;
+    /* color: red; */
+    color: rgb(197, 112, 1);
   }
   .newGame {
     font-size: 0.8rem;
-    background-color: rebeccapurple;
-    padding: .8rem 1.5rem;
+    background-color: rgb(154, 87, 0);
+    padding: 0.8rem 1.5rem;
     border: 0.2rem solid white;
     border-radius: 25px;
     text-shadow: 5px 5px 10px black;
     cursor: pointer;
     margin-top: 1rem;
   }
- @media (min-width: 821px) {
- main {
-    display: flex;
-    justify-content: center;
-    align-items: start;
-    padding: 2rem;
-    flex-direction: row-reverse;
+  @media (min-width: 821px) {
+    main {
+      display: flex;
+      justify-content: center;
+      align-items: start;
+      padding: 2rem;
+      flex-direction: row-reverse;
+    }
+    .bingo {
+      /* order: 1; */
+      display: grid;
+      grid-template-columns: repeat(6, 5rem);
+      margin: 0.5rem;
+    }
+    .bingo .first {
+      grid-column: 1 / -1;
+      border: 10px solid yellow;
+      font-size: 6rem;
+      inline-size: 6rem;
+      height: 6rem;
+      padding: 2.5rem;
+      text-align: center;
+      justify-self: center;
+      align-self: center;
+      margin-bottom: 1rem;
+      /* background-color: rgb(102, 0, 203); */
+      font-weight: bold;
+      text-shadow: 5px 5px 10px black;
+    }
+    .bingo span {
+      border: 3px solid rgba(255, 255, 0, 0.766);
+      border-radius: 50%;
+      color: white;
+      font-size: 1.5rem;
+      line-height: 1.5rem;
+      width: 1.5rem;
+      display: inline-block;
+      padding: 0.8rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 0.5rem;
+    }
+    section {
+      display: grid;
+      grid-template-columns: repeat(3, 200px);
+      flex: 1;
+      margin: 2rem 5rem;
+    }
+    .celda {
+      /* order: 2; */
+      width: 200px;
+      height: 200px;
+      border: 3px solid black;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 5.5rem;
+      text-shadow: 3px 3px 10px rgba(0, 0, 0, 0.736);
+    }
+    .ok {
+      /* background-color: rgb(73, 16, 131); */
+      /* border: 5px solid rgb(255, 255, 0); */
+      border-radius: 50%;
+      width: 75%;
+      height: auto;
+      aspect-ratio: 1/1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      /* font-weight: bold; */
+      /* box-shadow: 5px 5px 10px 2px black; */
+      /* font-size: 4.5rem; */
+    }
+    .winner {
+      width: 100%;
+      height: 100vh;
+      background-color: rgba(0, 0, 0, 0.8);
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+    .winner p {
+      font-size: 10rem;
+      letter-spacing: 1rem;
+      font-weight: bolder;
+      font-family: Arial, Helvetica, sans-serif;
+      -webkit-text-stroke: 0.2rem black;
+      text-shadow: 5px 5px 15px black;
+      /* color: red; */
+    }
+    .newGame {
+      font-size: 1.5rem;
+      /* background-color: rebeccapurple; */
+      padding: 1rem 2rem;
+      border: 0.2rem solid white;
+      border-radius: 25px;
+      text-shadow: 5px 5px 10px black;
+      cursor: pointer;
+    }
   }
-  .bingo {
-    /* order: 1; */
-    display: grid;
-    grid-template-columns: repeat(6, 5rem);
-    margin: 0.5rem;
-  }
-  .bingo .first {
-    grid-column: 1 / -1;
-    border: 10px solid yellow;
-    font-size: 6rem;
-    inline-size: 6rem;
-    height: 6rem;
-    padding: 2.5rem;
-    text-align: center;
-    justify-self: center;
-    align-self: center;
-    margin-bottom: 1rem;
-    background-color: rgb(102, 0, 203);
-    font-weight: bold;
-    text-shadow: 5px 5px 10px black;
-  }
-  .bingo span {
-    border: 3px solid rgba(255, 255, 0, 0.766);
-    border-radius: 50%;
-    color: white;
-    font-size: 1.5rem;
-    line-height: 1.5rem;
-    width: 1.5rem;
-    display: inline-block;
-    padding: 0.8rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0.5rem;
-  }
-  section {
-    display: grid;
-    grid-template-columns: repeat(3, 200px);
-    flex: 1;
-    margin: 2rem 5rem;
-  }
-  .celda {
-    /* order: 2; */
-    width: 200px;
-    height: 200px;
-    border: 3px solid rgb(255, 255, 0);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 5.5rem;
-    text-shadow: 3px 3px 10px rgba(0, 0, 0, 0.736);
-  }
-  .ok {
-    background-color: rgb(73, 16, 131);
-    border: 5px solid rgb(255, 255, 0);
-    border-radius: 50%;
-    width: 75%;
-    height: auto;
-    aspect-ratio: 1/1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-weight: bold;
-    box-shadow: 5px 5px 10px 2px black;
-    font-size: 4.5rem;
-  }
-  .winner {
-    width: 100%;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.8);
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-  }
-  .winner p {
-    font-size: 10rem;
-    letter-spacing: 1rem;
-    font-weight: bolder;
-    font-family: Arial, Helvetica, sans-serif;
-    -webkit-text-stroke: 0.2rem white;
-    text-shadow: 5px 5px 15px black;
-    color: red;
-  }
-  .newGame {
-    font-size: 1.5rem;
-    background-color: rebeccapurple;
-    padding: 1rem 2rem;
-    border: 0.2rem solid white;
-    border-radius: 25px;
-    text-shadow: 5px 5px 10px black;
-    cursor: pointer;
-  }
-}
 </style>
